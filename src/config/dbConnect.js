597a9acc5db621396486a8c15585faa5e26966c0
NaiDaros <135157103+NaiDaros@users.noreply.github.com>
2025-03-31 21:50:31 -0300

    Update dbConnect.js

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,6 @@
 import mongoose, { mongo } from "mongoose";
 
 async function connectToDatabase() {
-    // mongoose.connect("mongodb+srv://naiaradaros22:Jad220607*@cluster0.govz8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     mongoose.connect(process.env.DB_CONNECTION_STRING);
     return mongoose.connection;
 };
